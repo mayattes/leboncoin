53e497d33e9f34a662f6c3e3de3b8a2996e0f310
koumba koita <koumbakoita@MacBook-Air-de-koumba.local>
2018-02-06 16:28:52 +0100

    enregistrer

--- index.js
+++ index.js
@@ -2,7 +2,55 @@ var express = require('express');
 var multer = require("multer");
 var upload = multer({ dest: "public/uploads/" });
 var app = express();
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://10.90.0.34:27017/leboncoin-koumba');
+
+/* var studentSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+}); */
+
+var annonceSchema = new mongoose.Schema({
+    title: String,   
+    town: String,
+    description: String,
+    picture: String,
+    price: String,
+    picture: String,
+    town: String,
+    nickname: String,
+    email: String,
+    telephone: String
+
+});
+
+/* var Student = mongoose.model("Student", studentSchema); */
+var Annonce = mongoose.model("annonce", annonceSchema);
+
+/* var ann = new Annonce({
+    title: "Coffret blu-ray'Stanlay kubrick' neuf",
+    price: "20 €",
+    town: "Paris 75002",
+    description: "Vends coffret blu-ray 'Stanley-kubrick: un réalisateur visionnaire' le coffret inclu: -2001 : l'odyssée de l'espace, Eyes Wid Shut, Full Metal Jacket, Orange mécanique, Shinning, Lolita, Barry Lyndon"
+}); */
+
+
+
+/* ann.save(function (err, obj) {
+    if (err) {
+        console.log("something went wrong");
+    } else {
+        console.log("we just saved the new student " + obj.name);
+    }
+}); */
+
+/* Student.find({}, function (err, students) {
+    console.log(students);
+
+}); */
 
 
 app.get('/', function (req, res) {
@@ -10,13 +58,16 @@ app.get('/', function (req, res) {
 });
 
 
-app.post('/deposer', function (req, res) {
-    res.render('déposer.ejs');
+app.post('/deposer', upload.single("picture"), function (req, res) {
     var ann = new Annonce({
-        title: "Coffret blu-ray'Stanlay kubrick' neuf",
-        price: "20 €",
-        town: "Paris 75002",
-        description: "Vends coffret blu-ray 'Stanley-kubrick: un réalisateur visionnaire' le coffret inclu: -2001 : l'odyssée de l'espace, Eyes Wid Shut, Full Metal Jacket, Orange mécanique, Shinning, Lolita, Barry Lyndon"
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        picture: req.file.filename,
+        town: req.body.town,
+        nickname: req.body.nickname,
+        email: req.body.email,
+        telephone: req.body.telephone
     });
 
     ann.save(function (err, obj) {
@@ -24,6 +75,7 @@ app.post('/deposer', function (req, res) {
             console.log("something went wrong");
         } else {
             console.log("we just saved the new student " + obj.title);
+            res.redirect("/");
         }
     });
 });
@@ -40,47 +92,6 @@ app.post("/upload", upload.single("picture"), function (req, res) {
 
  app.listen(3000, function () {
     console.log('hello');
-});
-
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://10.90.0.34:27017/leboncoin-koumba');
-
-var studentSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-});
-
-var annonceSchema = new mongoose.Schema({
-    title: String,
-    price: String,
-    town: String,
-    description: String
-});
-
-var Student = mongoose.model("Student", studentSchema);
-var Annonce = mongoose.model("annonce", annonceSchema);
-
-var ann = new Annonce({
-    title : "Coffret blu-ray'Stanlay kubrick' neuf",
-    price : "20 €",
-    town: "Paris 75002",
-    description: "Vends coffret blu-ray 'Stanley-kubrick: un réalisateur visionnaire' le coffret inclu: -2001 : l'odyssée de l'espace, Eyes Wid Shut, Full Metal Jacket, Orange mécanique, Shinning, Lolita, Barry Lyndon"
-});
-
-
-
-ann.save(function (err, obj) {
-    if (err) {
-        console.log("something went wrong");
-    } else {
-        console.log("we just saved the new student " + obj.name);
-    }
-});
-
-Student.find({}, function (err, students) {
-    console.log(students);
-    
 });
 
 
